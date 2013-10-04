0c95c13c0f014c3b3ff053b162a0d3fcf961e031
Guy Vider <guy@guyvider.com>
2013-10-04 12:40:49 -0700

    changed button names; finished clear function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,52 +1,55 @@
 'use strict';
 
 requirejs.config({
-    shim: {
-        'bootstrap': {
-            deps: ['jquery']
-        }
-    },
-    paths: {
-        'jquery': [
-            '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-            'vendors/jquery-1.10.2.min'
-        ],
-        'knockout': 'vendors/knockout-2.3.0',
-        'bootstrap': 'vendors/bootstrap.min'
-    }
+	shim: {
+		'bootstrap': {
+			deps: ['jquery']
+		}
+	},
+	paths: {
+		'jquery': [
+			'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+			'vendors/jquery-1.10.2.min'
+		],
+		'knockout': 'vendors/knockout-2.3.0',
+		'bootstrap': 'vendors/bootstrap.min'
+	}
 });
 require(
-    [
-        'jquery',
-        'knockout',
-        'viewmodels/resultsVM',
-        'bootstrap'
-    ],
-    function($, ko, resultsVM) {
-        var vm = new resultsVM;
-        ko.applyBindings(vm);
+	[
+		'jquery',
+		'knockout',
+		'viewmodels/resultsVM',
+		'bootstrap'
+	],
+	function($, ko, resultsVM) {
+		var vm = new resultsVM;
+		ko.applyBindings(vm);
 
-        var submitForm = function(e) {
-            e.preventDefault();
-            $.post('/lint', $('form').serialize())
-                .done(function(data) {
-                    console.log(data);
-                    vm.populate(data);
-                })
-                .fail(function(jqXHR) {
-                    console.error(jqXHR.responseText);
-                })
-                .always(function() {
-                    $('#results').removeClass('hide');
-                });
-        };
+		var submitForm = function(e) {
+			e.preventDefault();
+			$.post('/lint', $('form').serialize())
+				.done(function(data) {
+					console.log(data);
+					vm.populate(data);
+				})
+				.fail(function(jqXHR) {
+					console.error(jqXHR.responseText);
+				})
+				.always(function() {
+					$('#results').removeClass('hide');
+				});
+		};
 
-        $(function() {
-            $('#submit').on('click', submitForm);
-            $('#clear').on('click', function(e) {
-                e.preventDefault();
-                $('textarea').val('');
-            });
-        });
-    }
+		var clearForm = function(e) {
+			e.preventDefault();
+			$('textarea').val('');
+			$('#results').addClass('hide');
+		};
+
+		$(function() {
+			$('#submitForm').on('click', submitForm);
+			$('#clearForm').on('click', clearForm);
+		});
+	}
 );
